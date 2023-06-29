8588a66ec3e2e5ad08c3c985e642819f4d47df81
olatomoloju <bksstech@gmail.com>
2023-06-29 17:40:22 +0100

    feature: test crud scenarios

--- src/main/java/com/bkss/capstone/crud/page/ClientPage.java
+++ src/main/java/com/bkss/capstone/crud/page/ClientPage.java
@@ -5,35 +5,59 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ClientPage {
-    private By clientName = By.id("name");
-    private By clientEmail = By.id("email");
-    private By goToAddClientLink = By.linkText("Add Client");
-    private By createClient = By.name("addClient");
-    private By newlyCreatedClient = By.xpath(".//*[@id='myTable']//td[contains(.,'Ola Tomoloju')]");
+    private final By clientName = By.id("name");
+    private final By clientEmail = By.id("email");
+    private final By goToAddClientLink = By.linkText("Add Client");
+    private final By createClient = By.xpath("//button[normalize-space()='Save']");
+    private final By editClient = By.xpath("//button[normalize-space()='Save']");
+    private final By deleteClient = By.xpath("//button[normalize-space()='Delete']");
+
     private WebDriver driver;
-    public ClientPage(WebDriver driver){
+
+    public ClientPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public void clickAddClientButton(){
+    public void clickAddClientButton() {
         driver.findElement(goToAddClientLink).click();
     }
 
-    public String addClient(Client client){
+    public String addClient(Client client) throws InterruptedException {
         clickAddClientButton();
-        //driver.findElement(clientName).sendKeys(client.getName());
-        //driver.findElement(clientEmail).sendKeys(client.getEmail());
-        driver.findElement(clientName).sendKeys("Ola Tomoloju");
-        driver.findElement(clientEmail).sendKeys("ola@gmail.com");
+        driver.findElement(clientName).sendKeys(client.getName());
+        driver.findElement(clientEmail).sendKeys(client.getEmail());
         driver.findElement(createClient).click();
+        synchronized (driver) {
+            driver.wait(1000);
+        }
+        By newlyCreatedClient = By.xpath(".//*[@id='clientsTable']//td[contains(.,'" + client.getName() + "')]");
         return driver.findElement(newlyCreatedClient).getText();
     }
 
-    public String editClient(Client client, String email){
-        return null;
+    public String editClient(Client client) throws InterruptedException {
+        By editedClient = By.xpath("//a[@href='/clients/4']");
+        driver.findElement(editedClient).click();
+        driver.findElement(clientName).clear();
+        driver.findElement(clientEmail).clear();
+        driver.findElement(clientName).sendKeys(client.getName());
+        driver.findElement(clientEmail).sendKeys(client.getEmail());
+        driver.findElement(editClient).click();
+        synchronized (driver) {
+            driver.wait(1000);
+        }
+        By editedClientName = By.xpath(".//*[@id='clientsTable']//td[contains(.,'" + client.getName() + "')]");
+        return driver.findElement(editedClientName).getText();
     }
 
-    public String deleteClient(Client client, String email){
-        return null;
+    public int deleteClient() throws InterruptedException {
+        driver.findElement(deleteClient).click();
+        synchronized (driver) {
+            driver.wait(1000);
+        }
+        return driver.findElements(By.xpath("//table[@id='clientsTable']/tbody/tr")).size();
+    }
+
+    public int currentRowCount() {
+        return driver.findElements(By.xpath("//table[@id='clientsTable']/tbody/tr")).size();
     }
 }
---
+++ src/test/java/com/bkss/capstone/crud/ClientTest.java
@@ -0,0 +1,57 @@
+package com.bkss.capstone.crud;
+
+import com.bkss.capstone.crud.domain.Client;
+import com.bkss.capstone.crud.page.ClientPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ClientTest {
+    private WebDriver driver;
+    private ClientPage clientPage;
+    private static final String URL = "http://localhost:3000";
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL);
+        driver.findElement(By.linkText("Click Me!")).click();
+        clientPage = new ClientPage(driver);
+    }
+
+    @Test
+    public void viewClientListTest() throws InterruptedException {
+        Assert.assertTrue(clientPage.currentRowCount() > 0);
+    }
+    @Test
+    public void createClientTest() throws InterruptedException {
+        Client newClient = new Client("Fname Lname","fname@gmail.com");
+        Assert.assertEquals(clientPage.addClient(newClient),newClient.getName());
+    }
+
+    @Test
+    public void editClientTest() throws InterruptedException {
+        Client editedClient = new Client("Edited Lname","edited@gmail.com");
+        Assert.assertEquals(clientPage.editClient(editedClient),editedClient.getName());
+    }
+
+    @Test
+    public void deleteClientTest() throws InterruptedException {
+        int currentRowsCount = clientPage.currentRowCount();
+        Assert.assertEquals(clientPage.deleteClient(),currentRowsCount-1);
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
